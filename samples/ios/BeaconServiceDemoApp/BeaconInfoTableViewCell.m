8ba9bb3196e294ef1d385cbd9ffd3b59d76ce977
Fabio Milano <fabio@touchwonders.com>
2015-07-15 10:01:29 +0200

    Fixed crash in case no longitude and/or latitude were provided when processing a beacon location dictionary.

--- samples/ios/BeaconServiceDemoApp/BeaconInfoTableViewCell.m
+++ samples/ios/BeaconServiceDemoApp/BeaconInfoTableViewCell.m
@@ -84,6 +84,9 @@
         }
     ];
   } else {
+    
+    beaconLocation = [self normalizedBeaconLocationFromBeaconLocationInfo:beaconLocation];
+      
     NSMutableAttributedString *text = [[NSMutableAttributedString alloc] initWithString:@""];
     [text appendAttributedString:[self makeTextMediumBold:@"Lat: "]];
     [text appendAttributedString:[[NSAttributedString alloc]
@@ -134,6 +137,44 @@
               value:[UIFont fontWithName:@"Helvetica-Bold" size:16.0]
               range:NSMakeRange(0, [str length])];
   return str;
+}
+
+#pragma mark Helper method
+
+/**
+ *  Convenience method for normaliz-ing a beacon location dictionary to properly conform to required values.
+ *
+ *  @param beaconLocation A beacon location info dictionary.
+ *
+ *  @return A normalized beacon location with missing values from in input beacon location replaced by default values.
+ *
+ *  @discuss Missing longitude and latitude values are replaced with default value: 0,0
+ */
+- (NSDictionary *)normalizedBeaconLocationFromBeaconLocationInfo:(NSDictionary *)beaconLocation
+{
+    if (!beaconLocation[@"longitude"] || !beaconLocation[@"latitude"]) {
+        // Received beacon location do not conform to required values.
+        // In case no values have been entered for latitude or longitutide "0,0" is inferred as default value.
+        
+        // Create an hougin for the normalized beacon location
+        NSMutableDictionary *normalizedBeaconLocation = [NSMutableDictionary dictionary];
+        
+        // Normalize longitude if needed
+        if (!beaconLocation[@"longitude"]) {
+            NSLog(@"A beacon location with longitude 0,0 is inferred");
+            [normalizedBeaconLocation setObject:@(0.0) forKey:@"longitude"];
+        }
+        
+        // Normalize latitude if needed
+        if (!beaconLocation[@"latitude"]) {
+            NSLog(@"A beacon location with longitude 0,0 is inferred");
+            [normalizedBeaconLocation setObject:@(0.0) forKey:@"latitude"];
+        }
+        
+        return [normalizedBeaconLocation copy];
+    }
+    
+    return beaconLocation;
 }
 
 @end
